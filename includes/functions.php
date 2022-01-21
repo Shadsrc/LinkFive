310e3138e6cff1d0bf4e6b5734cde476809b83bf
Shadsrc <shaddeh.pro@gmail.com>
2022-01-21 15:48:56 +0100

    Mise en place de l'apparence

--- includes/functions.php
+++ includes/functions.php
@@ -5,20 +5,23 @@
 
   function wplf_Add_My_Admin_Link()
   {
-  add_menu_page(
-    'LinkFive', // Titre de ma page
-    'LinkFive', // Texte sur le menu lien
-    'manage_options', // requis pour voir le lien
-    'PluginLinkedIn/includes/wplf-first-acp-page.php' // The 'slug' - file to display when clicking the link
-  );
+    add_menu_page(
+      'LinkFive', // Titre de ma page
+      'LinkFive', // Texte sur le menu lien
+      'manage_options', // requis pour voir le lien
+      'PluginLinkedIn/includes/wplf-first-acp-page.php' // The 'slug' - file to display when clicking the link
+    );
+    
   }
-
+  function wplf_Add_My_Submenu_Link(){
+  add_submenu_page('PluginLinkedIn/includes/wplf-first-acp-page.php','Login','login', 'manage_option','PluginLinkedIn/includes/wplf-second-acp-page.php');
+  }
   function style_LinkFive()
   {
-    wp_enqueue_style ('mystyle_LinkFive', plugins_url('/PluginLinkedIn/assets/css/style.css')); 
+    wp_enqueue_style ('mystyle_LinkFive', plugins_url('/PluginLinkedIn/assets/css/styleLinkFive.css')); 
   }
 
   // Appel du hook "action" sur admin menu, lance la fonction "wplf_Add_My_Admin_Link()"
   add_action( 'admin_menu', 'wplf_Add_My_Admin_Link' );
+  add_action( 'admin_menu', 'wplf_Add_My_Submenu_Link' );
   add_action('wp_enqueue_scripts', 'style_LinkFive');
-  
--- includes/wplf-second-acp-page.php
+++ includes/wplf-second-acp-page.php
@@ -3,21 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="wp-content/plugins/PluginLinkedIn/assets/css/styleLinkFive.css"/>
-
-        <!--Police d'écriture -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@500;700&display=swap" rel="stylesheet">
-
         <?php wp_head(); ?>
     </head>
 
     <body>
         <div class="wrap">
-            
-            <h1 class="title_LF">L__kFive</h1>
-            <p class="txt_Welcome">Bienvenue sur LinkFive ! Cliquez sur le bouton Login pour vous connecter a LinkedIn.</p>
+            <h1 class="title_LF"></h1>
+            <p class="txt_Welcome"></p>
             <button class="buttonLogin" type="button">Login</button>
-            
-
         </div>
     </body>
 </html>
